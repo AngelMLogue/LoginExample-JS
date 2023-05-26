434f70a557748a022b9b2b649382b1287fc1c13d
Digitalizacion TM <Digitalizacion TM@TM-ANGELLOERA>
2023-05-26 10:52:21 -0600

    Add project files.

---
+++ LoginExample/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using LoginExample.Permisos;
+
+namespace LoginExample.Controllers
+{
+    [ValidarSesion]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        public ActionResult CerrarSesion()
+        {
+            Session["usuario"] = null;
+            return RedirectToAction("Login", "Acceso");
+        }
+
+
+    }
+}
